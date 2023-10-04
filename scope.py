c1678caf4f5ebe383c45cf49642cd3b3a339cb8a
Ellis velandia <eyis619@gmail.com>
2023-10-03 20:20:17 -0500

    scope

---
+++ scope.py
@@ -0,0 +1,15 @@
+name = "Ellis"
+count = 1
+
+def another():
+ color = "red"
+ count = 2
+ print(count)
+ def greeting(name):
+     print(color)
+     print(name)
+
+ greeting("Ellis caicedo") 
+
+
+another()    
