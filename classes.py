b8aeb5fe1aaa98820a4d4148f0d73be0f75555ce
Ellis velandia <eyis619@gmail.com>
2023-10-04 20:31:09 -0500

    classes

---
+++ classes.py
@@ -0,0 +1,57 @@
+class Vehicule:
+    def __init__(self, make ,model):
+        self.make = make
+        self.model = model
+
+    def moves(self):
+        print("moves along...")
+
+    def get_make_model(self):
+        print(f"I'm a {self.make} {self.model}")    
+        
+
+my_car = Vehicule("Tesla", "model 3")
+
+# print(my_car.make)
+# print(my_car.model)
+my_car.get_make_model()
+my_car.moves()
+
+your_car = Vehicule("Cadillac", "Escalade")
+your_car.get_make_model()
+your_car.moves()
+
+class Airplane(Vehicule):
+     def __init__(self, make ,model, faa_id):
+        super().__init__(make, model)
+        self.faa_id = faa_id
+
+     def moves(self):
+         print("Flies along...")
+
+class Truck(Vehicule):
+    def moves(self):
+        print("Rumbles along...")
+
+class GolfCart(Vehicule):
+    pass
+
+cessna = Airplane("Cessna", "Skyhawk", "N-12345")
+mack = Truck("Mack", "Pinnacle")
+golfwagon = GolfCart("Yamaha", "Gc100")
+
+cessna.get_make_model()
+cessna.moves()
+
+mack.get_make_model()
+mack.moves()
+
+golfwagon.get_make_model()
+golfwagon.moves()
+
+print('\n\n')
+
+for v in (my_car, your_car,  cessna ,mack , golfwagon):
+    v.get_make_model()
+    v.moves()
+
